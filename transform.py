4d8496f8554c74be1a847eb4a5e18d44643135b9
Ken Chan <nok4123456@gmail.com>
2023-11-24 14:43:27 +0800

    Update transform.py
    Data will upload the the google storage

--- transform.py
+++ transform.py
@@ -1,15 +1,16 @@
 # %%
-
-from dotenv import load_dotenv
 from chain import init_chain
+from cloudpathlib import GSPath
 import polars as pl
 import logging
 
 
 def main():
-    load_dotenv()
+    GS_FILE_PATH = GSPath(
+        "gs://ken_chan_personal_project/gpt_bbc_project/transformed_news.csv"
+    )
     chain = init_chain()
-    original_news_df = pl.read_csv("./news.csv")
+    original_news_df = pl.read_csv("./data/news.csv")
     transformed_news_list = []
     for row in original_news_df.rows(named=True):
         docs = row["content"]
@@ -19,7 +20,8 @@ def main():
         row["summary"] = res
         transformed_news_list.append(row)
     transformed_news_df = pl.DataFrame(transformed_news_list)
-    transformed_news_df.write_csv("./transformed_news.csv")
+    transformed_news_df.write_csv("./data/transformed_news.csv")
+    GS_FILE_PATH.upload_from("./data/transformed_news.csv")
     return
 
 
